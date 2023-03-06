117c0e82913c283da8feb42fa971558ee84cf7a4
Cassie Steighner <steighnercr@gmail.com>
2023-03-06 15:44:49 -0500

    Create post status

--- StatusSolution/Status/Program.cs
+++ StatusSolution/Status/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Marten;
 using Status;
 
 //Console.WriteLine("This is a .NET Application");
@@ -14,12 +15,29 @@ using Status;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// configure "services" - Entities, Values, Services
+var connectionString = "host=localhost;database=status_dev;username=postgres;password=TokyoJoe138!;port=5432";
+builder.Services.AddMarten(options =>
+{
+    options.Connection(connectionString);
+    options.AutoCreateSchemaObjects = Weasel.Core.AutoCreate.All;
+});
+
 var app = builder.Build();
 
 app.MapGet("/status", () =>
 {
     var statusMessage = new StatusMessage(Guid.NewGuid(), "Looks Good", DateTimeOffset.Now);
     return Results.Ok(statusMessage);
+});
+
+app.MapPost("/status", async (StatusChangeRequest request, IDocumentSession db) =>
+{
+    // save message in database
+    var messageToSave = new StatusMessage(Guid.NewGuid(), request.Message, DateTimeOffset.Now);
+    db.Store<StatusMessage>(messageToSave);
+    await db.SaveChangesAsync(); 
+    return Results.Ok(messageToSave);
 });
 
 app.Run(); // "Block"
