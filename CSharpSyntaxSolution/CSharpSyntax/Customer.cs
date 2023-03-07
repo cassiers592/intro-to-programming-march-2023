ab9e585f64426cdd30a8a1d5e028072c22751f51
Cassie Steighner <steighnercr@gmail.com>
2023-03-07 14:41:37 -0500

    Create simple Customer class to show OOOP

---
+++ CSharpSyntaxSolution/CSharpSyntax/Customer.cs
@@ -0,0 +1,24 @@
+﻿
+namespace CSharpSyntax;
+
+public class Customer
+{
+    private decimal _availableCredit = 5000;
+    /// <summary>
+    /// Use this method to get the current available credit
+    /// </summary>
+    /// <returns>what the customer's credit is right now</returns>
+    public decimal GetCurrentAvailableCredit()
+    {
+        return _availableCredit;
+    }
+
+    /// <summary>
+    /// Use this method to increase the credit limit
+    /// </summary>
+    /// <param name="amount">The amount to add to the current credit limit</param>
+    public void IncreaseAvailableCredit(decimal amount)
+    {
+        _availableCredit += amount;
+    }
+}
