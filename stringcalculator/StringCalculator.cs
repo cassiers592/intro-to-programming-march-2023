d0cf27814e0f1088bf695c704772e63f0e1feb45
Cassie Steighner <steighnercr@gmail.com>
2023-03-08 16:01:15 -0500

    a few different ways to do things

--- stringcalculator/StringCalculator.cs
+++ stringcalculator/StringCalculator.cs
@@ -7,20 +7,26 @@ public class StringCalculator
     public int Add(string numbers)
     {
         if (numbers == "") return 0;
-        var delimeter = ',';
+        //var delimeter = ',';
+        var delimeters = new List<char> { ',', '\n' };
         if (numbers.StartsWith("//"))
         {
-            delimeter = numbers[2];
+            //delimeter = numbers[2];
+            delimeters.Add(numbers[2]);
             numbers = numbers.Substring(4);
         }
-        numbers = numbers.Replace('\n', delimeter);
+        //numbers = numbers.Replace('\n', delimeter);
         var sum = 0;
-        var numberList = numbers.Split(delimeter);
+        //var numberList = numbers.Split(delimeter);
+        var numberList = numbers.Split(delimeters.ToArray());
 
         foreach (var number in numberList)
         {
             sum += int.Parse(number);
         }
         return sum; 
+
+        // OR
+        //return numberList.Select(int.Parse).Sum();
     }
 }
