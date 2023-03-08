131c6ea4ee9e84df4ce5d138060f0d5e22a49121
Cassie Steighner <steighnercr@gmail.com>
2023-03-08 15:04:00 -0500

    Create String Calculator using TDD https://osherove.com/tdd-kata-1 Through number 4

---
+++ stringcalculator/stringcalculator-master/StringCalculator.cs
@@ -0,0 +1,26 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        if (numbers == "") return 0;
+        var delimeter = ',';
+        if (numbers.StartsWith("//"))
+        {
+            delimeter = numbers[2];
+            numbers = numbers.Substring(4);
+        }
+        numbers = numbers.Replace('\n', delimeter);
+        var sum = 0;
+        var numberList = numbers.Split(delimeter);
+
+        foreach (var number in numberList)
+        {
+            sum += int.Parse(number);
+        }
+        return sum; 
+    }
+}
---
+++ stringcalculator/stringcalculator-master/StringCalculatorTests.cs
@@ -0,0 +1,77 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("4", 4)]
+    [InlineData("5", 5)]
+    [InlineData("1234", 1234)]
+    public void SingleDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("2,3", 5)]
+    [InlineData("4,5", 9)]
+    [InlineData("12,340", 352)]
+    public void TwoDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("2,3,4,5", 14)]
+    [InlineData("12,340,1", 353)]
+    public void ManyDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("2\n3,4\n5", 14)]
+    [InlineData("12\n340\n1", 353)]
+    public void NewLineSplitter(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("//;\n1;2", 3)]
+    [InlineData("//x\n12\n340x1", 353)]
+    public void DifferentDelimiters(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+}
