00ba9b92eaaeda7b9fffcc6bf00defc65816655a
Cassie Steighner <steighnercr@gmail.com>
2023-03-09 12:23:08 -0500

    Test bank account uses the bonus calculator correctly

---
+++ BankingSolution/Banking.UnitTests/BankAccountUsesBonusCalculator.cs
@@ -0,0 +1,18 @@
+﻿
+using Banking.Domain;
+using Banking.UnitTests.TestDoubles;
+
+namespace Banking.UnitTests;
+
+public class BankAccountUsesBonusCalculator
+{
+    [Fact]
+    public void IntegratesBonusCalculator()
+    {
+        var bankAccount = new BankAccount(new StubbedBonusCalculator());
+
+        bankAccount.Deposit(212.83M);
+
+        Assert.Equal(5224.83M, bankAccount.GetBalance());
+    }
+}
