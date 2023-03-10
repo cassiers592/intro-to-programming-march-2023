9c22d4bbea6881c11f4ae01186c17622bc4742ca
Cassie Steighner <steighnercr@gmail.com>
2023-03-10 15:51:12 -0500

    class info, records, properties etc. w/ bank account ex

---
+++ CSharpSyntaxSolution/CSharpSyntax/BankAccount.cs
@@ -0,0 +1,75 @@
+﻿
+namespace CSharpSyntax;
+
+public class BankAccount
+{
+    private string _accountNumber = string.Empty;
+    private string _firstName = string.Empty;
+    public string LastName { get; private set; } = string.Empty;
+
+    internal BankAccount(string accountNumber)
+    {
+        _accountNumber = accountNumber;
+    }
+
+    public string GetAccountNumber()
+    {
+        return _accountNumber;
+    }
+
+    //public string GetFirstName()
+    //{
+    //    return _firstName;
+    //}
+
+    //public void SetFirstName (string firstName)
+    //{
+    //    _firstName = firstName;
+    //}
+
+    // Properties
+    public string FirstName
+    {
+        get { return _firstName; }
+        set { _firstName = value; }
+    }
+}
+
+// Class that just does some work for me
+// Services
+public class AccountManager
+{
+    public BankAccount GetAccountByID(string accountNumber)
+    {
+        // go find in the database
+        var account = new BankAccount(accountNumber);
+        account.FirstName = "Jeff";
+        return account;
+    }
+}
+
+public enum TransactionType { Deposit, Withdrawl, Fee}
+
+// In order to compare you have to implement IEquatable
+//public class BankTransaction
+//{
+//    public required TransactionType TransactionType { get; init; }
+//    public required decimal Amount { get; init; }
+//    public string Note { get; init; } = string.Empty;
+//}
+
+// records automatically implement IEquatable
+public record BankTransaction
+{
+    public required TransactionType TransactionType { get; init; }
+    public required decimal Amount { get; init; }
+    public string Note { get; init; } = string.Empty;
+}
+
+public record Song(string Title, string Artist, string Album);
+public record Song2
+{
+    public string Title { get; init; } = string.Empty;
+    public string Artist { get; init; } = string.Empty;
+    public string Album { get; init; } = string.Empty;
+}
---
+++ CSharpSyntaxSolution/CSharpSyntax/BankAccountTests.cs
@@ -0,0 +1,55 @@
+﻿
+namespace CSharpSyntax;
+
+public class BankAccountTests
+{
+    [Fact]
+    public void GettingMyAccount()
+    {
+        var service = new AccountManager();
+        var myAccount = service.GetAccountByID("93939");
+
+        Assert.Equal("93939", myAccount.GetAccountNumber());
+
+        Assert.Equal("Jeff", myAccount.FirstName);
+
+        myAccount.FirstName = "Jeffry";
+
+        Assert.Equal("Jeffry", myAccount.FirstName);
+
+    }
+
+    [Fact]
+    public void WorkingWithTransactions()
+    {
+        var transaction = new BankTransaction
+        {
+            TransactionType = TransactionType.Deposit,
+            Amount = 5000
+        };
+
+        // send it to the bank
+
+        var transaction2 = new BankTransaction
+        {
+            TransactionType = TransactionType.Deposit,
+            Amount = 5000
+        };
+
+        Assert.Equal(transaction, transaction2);
+
+        var transaction3 = transaction with { Amount = 1000, Note = transaction.Note + "Work" };
+    }
+
+    [Fact]
+    public void SongExample()
+    {
+        var song1 = new Song("ABCs", "Unknown", "Unknown");
+        var song2 = new Song2
+        {
+            Title = "Twinkle Twinkle",
+            Artist = "Unknown",
+            Album = "Unknown"
+        };
+    }
+}
