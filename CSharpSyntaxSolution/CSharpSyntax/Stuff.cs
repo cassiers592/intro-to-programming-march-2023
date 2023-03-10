b643c07347e7149ed73e91dff59313a6a6f613b8
Cassie Steighner <steighnercr@gmail.com>
2023-03-10 14:42:09 -0500

    basicc class/method info

---
+++ CSharpSyntaxSolution/CSharpSyntax/Stuff.cs
@@ -0,0 +1,16 @@
+﻿
+namespace CSharpSyntax;
+
+public class Stuff
+{
+    private int x = 20;
+    public void DoIt()
+    {
+        x -= 1;
+    }
+
+    public int GetX()
+    {
+        return x;
+    }
+}
---
+++ CSharpSyntaxSolution/CSharpSyntax/WorkingWithMethods.cs
@@ -0,0 +1,24 @@
+﻿
+namespace CSharpSyntax;
+
+public class WorkingWithMethods
+{
+    [Fact]
+    public void WorkingWithStuff()
+    {
+        var o1 = new Stuff();
+        o1.DoIt();
+
+        var o2 = new Stuff();
+        o2.DoIt();
+
+        Assert.Equal(19, o1.GetX());
+        Assert.Equal(19, o2.GetX());
+
+        o2.DoIt();
+        o2.DoIt();
+
+        Assert.Equal(19, o1.GetX());
+        Assert.Equal(17, o2.GetX());
+    }
+}
