cb547ddb7b23c8b38b484486ad5a3fc955631dc9
Cassie Steighner <steighnercr@gmail.com>
2023-03-13 12:03:20 -0400

    create dashboard and navigation components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MastheadComponent } from './components/masthead/masthead.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MastheadComponent
+    MastheadComponent,
+    DashboardComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
