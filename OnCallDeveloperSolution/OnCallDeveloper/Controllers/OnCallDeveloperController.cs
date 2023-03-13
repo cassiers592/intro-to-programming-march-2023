39bd74a5032f0e1bfc75da42e902b1ac30085189
Cassie Steighner <steighnercr@gmail.com>
2023-03-13 16:42:52 -0400

    add response cache

--- OnCallDeveloperSolution/OnCallDeveloper/Controllers/OnCallDeveloperController.cs
+++ OnCallDeveloperSolution/OnCallDeveloper/Controllers/OnCallDeveloperController.cs
@@ -14,6 +14,7 @@ public class OnCallDeveloperController : ControllerBase
 
     // GET /oncalldeveloper -> 200 Ok
     [HttpGet("/oncalldeveloper")]
+    [ResponseCache(Location=ResponseCacheLocation.Any, Duration = 30)]
     public ActionResult GetOnCallDeveloper()
     {
         GetOnCallDeveloperResponse response;
