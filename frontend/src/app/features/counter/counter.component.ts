120006c6f3978ba1b04f035cef8ba1374a9ab356
Cassie Steighner <steighnercr@gmail.com>
2023-03-14 14:12:49 -0400

    TODO: Redux

--- frontend/src/app/features/counter/counter.component.ts
+++ frontend/src/app/features/counter/counter.component.ts
@@ -6,13 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./counter.component.css'],
 })
 export class CounterComponent {
+  // TODO: make this an observable of some data in the store
   current = 0;
 
   increment() {
+    // TODO: dispatch an action to the store to say that the count was incremented
     this.current += 1;
   }
 
   decrement() {
+    // TODO: dispatch an action to the store to say that the count was decremented
     this.current -= 1;
   }
 }
