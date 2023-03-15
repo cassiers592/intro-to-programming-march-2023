820dbbc6cc590023424031996b5ad70f8888c798
Cassie Steighner <steighnercr@gmail.com>
2023-03-15 15:11:51 -0400

    creating learning children

--- frontend/src/app/features/learning/learning.module.ts
+++ frontend/src/app/features/learning/learning.module.ts
@@ -2,16 +2,44 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LearningComponent } from './learning.component';
 import { RouterModule, Routes } from '@angular/router';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ListComponent } from './components/list/list.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { NewComponent } from './components/new/new.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LearningComponent,
+    children: [
+      {
+        path: 'overview',
+        component: OverviewComponent,
+      },
+      {
+        path: 'list',
+        component: ListComponent,
+      },
+      {
+        path: 'new',
+        component: NewComponent,
+      },
+      {
+        path: '**',
+        component: OverviewComponent,
+      },
+    ],
   },
 ];
 
 @NgModule({
-  declarations: [LearningComponent],
+  declarations: [
+    LearningComponent,
+    NavigationComponent,
+    ListComponent,
+    OverviewComponent,
+    NewComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
