e5fb6a9b674d6f2d3624604a9baa428ef7758a9e
Cassie Steighner <steighnercr@gmail.com>
2023-03-15 09:15:06 -0400

    create counter cypress tests

---
+++ frontend/cypress/e2e/counter.cy.ts
@@ -0,0 +1,33 @@
+describe('The Counter Page', () => {
+  describe('The Counter', () => {
+    beforeEach(() => {
+      cy.visit('/counter');
+    });
+
+    it('starts with counter at 0', () => {
+      cy.get('[data-testid="current-count"]').should('contain.text', '0');
+    });
+
+    it('increments the counter when the "+" is clicked', () => {
+      cy.get('[data-testid="increment-btn"]').click();
+      cy.get('[data-testid="current-count"]').should('contain.text', '1');
+    });
+
+    it('decrements the counter when the "-" is clicked', () => {
+      cy.get('[data-testid="decrement-btn"]').click();
+      cy.get('[data-testid="current-count"]').should('contain.text', '-1');
+    });
+
+    it('resets the counter when the "reset" button is clicked', () => {
+      cy.get('[data-testid="increment-btn"]').click();
+      cy.get('[data-testid="reset-btn"]').click();
+      cy.get('[data-testid="current-count"]').should('contain.text', '0');
+    });
+
+    it('counts by 5 when the "5" is clicked', () => {
+      cy.get('[data-testid="count-by"]').find('[data-testid="5"]').click();
+      cy.get('[data-testid="increment-btn"]').click();
+      cy.get('[data-testid="current-count"]').should('contain.text', '5');
+    });
+  });
+});
