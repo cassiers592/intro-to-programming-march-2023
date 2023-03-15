d147ee0f49d70301a5bc5f60021d78ce96d9ddac
Cassie Steighner <steighnercr@gmail.com>
2023-03-15 11:52:36 -0400

    handle invalid local storage

--- frontend/src/app/features/counter/state/effects/counter.effects.ts
+++ frontend/src/app/features/counter/state/effects/counter.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Actions, concatLatestFrom, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { map, tap, filter } from 'rxjs';
+import { map, tap, filter, catchError, of } from 'rxjs';
 import { selectCounterBranch } from '..';
 import {
   counterCommands,
@@ -9,9 +9,19 @@ import {
   counterEvents,
 } from '../actions/counter.actions';
 import { CounterState } from '../reducers/counter.reducer';
+import { z } from 'zod';
 
 @Injectable()
 export class CounterEffects {
+  private readonly CountDataSchema = z.object({
+    current: z.number(),
+    by: z.union([
+      z.literal(1),
+      z.literal(3),
+      z.literal(5),
+    ]),
+  });
+
   // logItAll$ = createEffect(
   //   () => {
   //     return this.actions$.pipe(tap((action) => console.log(action.type))); // =>
@@ -26,10 +36,16 @@ export class CounterEffects {
       ofType(counterCommands.loadCounterState),
       map(() => localStorage.getItem('counter-state')), // string or null
       filter((storedValue) => storedValue !== null), // stop here if it's null
-      map((theString) => JSON.parse(theString!) as CounterState), // ! tells the compiler that theString is not null
+      map((theString) => JSON.parse(theString!)), // ! tells the compiler that theString is not null
+      map((susObject) => this.CountDataSchema.parse(susObject) as CounterState),
       map((counterState) =>
         counterDocuments.counterState({ payload: counterState }),
       ),
+      catchError(() => {
+        console.log('We have ourselves a hacker here!');
+        localStorage.clear();
+        return of({ type: 'Localstorage Error' });
+      }),
     );
   });
 
