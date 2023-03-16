0f89404ae61785f0f89818d8abc43913daa3af08
Cassie Steighner <steighnercr@gmail.com>
2023-03-16 11:01:06 -0400

    form validation

--- frontend/src/app/features/learning/components/new/new.component.ts
+++ frontend/src/app/features/learning/components/new/new.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ItemEntityRequestModel } from '../../models/items.models';
 
 @Component({
   selector: 'app-new',
@@ -7,15 +8,46 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./new.component.css'],
 })
 export class NewComponent {
-  form = new FormGroup({
-    name: new FormControl(),
-    description: new FormControl(),
-    link: new FormControl(),
+  form = new FormGroup<FormDataType<ItemEntityRequestModel>>({
+    name: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.maxLength(50),
+      ],
+    }),
+    description: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.maxLength(100)],
+    }),
+    link: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+      ],
+    }),
   });
+
+  get name() {
+    return this.form.controls.name;
+  }
+  get description() {
+    return this.form.controls.description;
+  }
+  get link() {
+    return this.form.controls.link;
+  }
 
   addIt() {
     // if the form is valid, dispatch an action
-    console.log(this.form.valid);
-    console.log(this.form.value);
+    if (this.form.valid) {
+      // TODO: Dispatch an Action
+    } else {
+      // TODO:
+    }
   }
 }
+
+type FormDataType<T> = {
+  [Property in keyof T]: FormControl<T[Property]>;
+};
