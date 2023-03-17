2d709800a3ad57de2f24a1ba7206a12d2f1243d9
Cassie Steighner <steighnercr@gmail.com>
2023-03-17 09:44:43 -0400

    separate learning tests

--- frontend/cypress/e2e/learning/list.cy.ts
+++ frontend/cypress/e2e/learning/list.cy.ts
@@ -1,25 +1,4 @@
 describe('The Learning Resources Page', () => {
-  describe('The Learning Navigation', () => {
-    beforeEach(() => {
-      cy.visit('/learning');
-    });
-
-    it('navigates to the overview when "Overview" is clicked', () => {
-      cy.get('[data-testid="overview-btn"]').click();
-      cy.url().should('contain', '/learning/overview');
-    });
-
-    it('navigates to the list when "List" is clicked', () => {
-      cy.get('[data-testid="list-btn"]').click();
-      cy.url().should('contain', '/learning/list');
-    });
-
-    it('navigates to the new when "Add a Resource" is clicked', () => {
-      cy.get('[data-testid="new-btn"]').click();
-      cy.url().should('contain', '/learning/new');
-    });
-  });
-
   describe('The List Page', () => {
     beforeEach(() => {
       cy.intercept(
